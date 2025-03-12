3c3772ffc597d9712c8476782b86d37ef0a52cd2
Arseniy_Yolkin <arsreniy.yolkin@yandex.ru>
2025-03-12 17:10:37 +0500

    task completed

---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,14 @@
+export type MockServerData = {
+  titles: string[];
+  descriptions: string[];
+  cities: string[];
+  previewPhotos: string[];
+  photos: string[];
+  housingTypes: string[];
+  amenities: string[];
+  authorNames: string[];
+  authorEmails: string[];
+  authorAvatars: string[];
+  authorTypes: string[];
+  coordinates: string[];
+};
