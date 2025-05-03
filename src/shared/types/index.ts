e837770ddc8e81b3c201f4b00709ab5ac43478b1
Arseniy_Yolkin <arsreniy.yolkin@yandex.ru>
2025-05-03 16:53:52 +0500

    task done

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -6,3 +6,4 @@ export * from './place-coordinates.type.js';
 export * from './user-type.enum.js';
 export * from './user.type.js';
 export * from './mock-server-data.type.js';
+export * from './component.enum.js';
